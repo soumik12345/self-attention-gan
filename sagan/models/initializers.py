4662870bc507592be95d92117a08b75eb5e64d30
soumik12345 <19soumik.rakshit96@gmail.com>
2021-11-01 21:48:56 +0530

    Added spectral normalization

--- sagan/models/initializers.py
+++ sagan/models/initializers.py
@@ -3,8 +3,8 @@ from tensorflow.keras import initializers
 
 
 class L2RandomNormal(initializers.TruncatedNormal):
-    def __init__(self, epsilon=1e-2, seed=None):
-        super(L2RandomNormal, self).__init__(mean=0.0, stddev=1.0, seed=seed)
+    def __init__(self, epsilon=1e-2, mean=0.0, stddev=1.0, seed=None):
+        super(L2RandomNormal, self).__init__(mean=mean, stddev=stddev, seed=seed)
         self.epsilon = epsilon
 
     def __call__(self, shape, dtype=tf.float32):
