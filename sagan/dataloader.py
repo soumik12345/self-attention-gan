ac2e11c4242608418e84de81aa7fd073f4c35d7e
soumik12345 <19soumik.rakshit96@gmail.com>
2021-11-06 17:15:50 +0530

    Added dataloader

---
+++ sagan/dataloader.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+
+class DataLoader:
+
+    def __init__(self, image_size: int = 64) -> None:
+        self.image_size = image_size
+
+    def _load_data(self, data):
+        image = tf.cast(data["image"], dtype=tf.float32)
+        image = tf.image.resize(images=image, size=[self.image_size, self.image_size])
+        image = (image / 127.0) - 1.0
+        return image
+
+    def get_dataset(self, dataset_name: str, split: str = "train", batch_size: int = 16):
+        dataset = tfds.load(name=dataset_name, split=split, shuffle_files=True)
+        dataset = dataset.map(self._load_data, num_parallel_calls=tf.data.AUTOTUNE).cache()
+        dataset = dataset.shuffle(10 * batch_size).batch(batch_size, drop_remainder=True)
+        dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+        return dataset
