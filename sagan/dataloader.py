20c132564b32df462529d938625016b9822407aa
soumik12345 <19soumik.rakshit96@gmail.com>
2021-11-06 17:32:59 +0530

    Added dataloader testcase

--- sagan/dataloader.py
+++ sagan/dataloader.py
@@ -2,10 +2,9 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 
 
-
 class DataLoader:
-
-    def __init__(self, image_size: int = 64) -> None:
+    def __init__(self, dataset_name: str, image_size: int = 64) -> None:
+        self.dataset_name = dataset_name
         self.image_size = image_size
 
     def _load_data(self, data):
@@ -14,9 +13,13 @@ class DataLoader:
         image = (image / 127.0) - 1.0
         return image
 
-    def get_dataset(self, dataset_name: str, split: str = "train", batch_size: int = 16):
-        dataset = tfds.load(name=dataset_name, split=split, shuffle_files=True)
-        dataset = dataset.map(self._load_data, num_parallel_calls=tf.data.AUTOTUNE).cache()
-        dataset = dataset.shuffle(10 * batch_size).batch(batch_size, drop_remainder=True)
+    def get_dataset(self, split: str = "train", batch_size: int = 16):
+        dataset = tfds.load(name=self.dataset_name, split=split, shuffle_files=True)
+        dataset = dataset.map(
+            self._load_data, num_parallel_calls=tf.data.AUTOTUNE
+        ).cache()
+        dataset = dataset.shuffle(10 * batch_size).batch(
+            batch_size, drop_remainder=True
+        )
         dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
         return dataset
